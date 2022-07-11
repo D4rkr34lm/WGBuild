813d9f9b115d43922ea230cf1cf5eb0a363f3c11
Prinz-Autistimus <handydandy2004@gmail.com>
2022-07-11 21:00:07 +0200

    Changes: - less rounding of Trail-Information - Added Position to Trail-Information

--- src/main/java/com/d4rkr34lm/wgbuild/trail/TrailClickListener.java
+++ src/main/java/com/d4rkr34lm/wgbuild/trail/TrailClickListener.java
@@ -2,6 +2,7 @@ package com.d4rkr34lm.wgbuild.trail;
 
 import com.d4rkr34lm.wgbuild.WGBuild;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
@@ -23,9 +24,15 @@ public class TrailClickListener implements Listener {
         Entity clicked = e.getRightClicked();
         TrailObject information = WGBuild.getLookupTable().get(clicked);
         Vector velocity = information.getVelocity();
-        double velX = Math.round((velocity.getX()*1000.0))/1000.0;
-        double velZ = Math.round((velocity.getZ()*1000.0))/1000.0;
-        double velY = Math.round((velocity.getY()*1000.0))/1000.0;
+        Location location = information.getLocation();
+
+        double velX = velocity.getX();
+        double velZ = velocity.getZ();
+        double velY = velocity.getY();
+
+        double posX = location.getX();
+        double posZ = location.getZ();
+        double posY = location.getY();
 
         Bukkit.broadcastMessage("\n"
                         + INFORMATION_ABOUT_ENTITY + "\n"
@@ -34,6 +41,10 @@ public class TrailClickListener implements Listener {
                         + "     §6X: " + velX + " blocks/sec \n"
                         + "     §6Z: " + velZ + " blocks/sec \n"
                         + "     §6Y: " + velY + " blocks/sec \n"
+                        + "§aPosition: " + "\n"
+                        + "     §6X: " + posX + "\n"
+                        + "     §6Z: " + posZ + "\n"
+                        + "     §6Y: " + posY + "\n"
                         + "§aExploded: §6" + information.getExplosion());
 
 
